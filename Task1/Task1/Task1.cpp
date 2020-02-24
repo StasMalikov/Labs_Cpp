018d4d5924162be68a9b7b1b83c6104f7e13299c
stas_malikov <stas.malikov75@gmail.com>
2020-02-24 11:10:46 +0300

    task1 - на сдачу

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -4,13 +4,37 @@
 #include <locale.h>
 #pragma warning(disable : 4996)
 
+// Маликов С.А.
+
+//http://all-ht.ru/inf/prog/c/func/memccpy.html
+//Аргументы:
+//
+//		 destination – указатель на массив в который будут скопированы данные.
+//			 source – указатель на массив источник копируемых данных.
+//			 c – код символа, при встрече которого копирование завершается.
+//			 n – количество байт для копирования.
+//
+//			 Возвращаемое значение :
+//
+//		 Функция возвращает указатель на массив, в который скопированы данные,
+//					если скопировано n байт.
+//			 NULL если в первых n байтах массива источника встречен символ, 
+//						код которого соответствует указанному в аргументе c.
+//
+//			 Описание :
+//
+//			 Функция memccpy копирует данные из массива(области памяти), на который 
+//				указывает аргумент source, в массив(область памяти), на который указывает аргумент 
+//				destination пока не встретится символ, код которого соответствует указанному в аргументе c
+//				или пока не будет скопировано n байт данных.
+
 
 char* my_memccpy(char* destination, const char* source, int c, size_t n) {
 	for (int i = 0; i < n; i++)
 	{
 		destination[i] = *(source + i);
 		if (*(source + i) == c) {
-			break;
+			return NULL;
 		}
 	}
 	return destination;
@@ -19,49 +43,33 @@ char* my_memccpy(char* destination, const char* source, int c, size_t n) {
 int main(void) {
 	setlocale(LC_ALL, "Russian");
 
-	//char buffer[61];
-	//int *pdest;
-	//char string1[60] = "The quick brown dog jumps over the lazy fox";
-	//printf("Source: %s\n", string1);
-	//pdest = (int *)(memccpy(buffer, string1, 's', 20));
-	//*pdest = '\0';
-	//printf("Result: %s\n", buffer);
-	//printf("Length: %d characters\n", strlen(buffer));
+	char source[60] = "text_f_text";
+	printf("Массив из которого копируем: %s\n\n", source);
 
-	// 1
-	/*char buffer[61] = "";
-	char string1[60] = "textftext";
-	memccpy(buffer, string1, 'f', 30);
-	printf("Result: %s\n", buffer);*/
-
+	//1
+	printf("Пример 1\n");
+	char destination1_1[60] = "";
+	char destination1_2[60] = "";
+	memccpy(destination1_1, source, 'f', 30);
+	printf("Результат встроенной функции: %s\n", destination1_1);
+	my_memccpy(destination1_2, source, 'f', 30);
+	printf("Результат самописной функции: %s\n\n", destination1_2);
 
 	// 2 
-	/*char buffer[61] = "12345678";
-	char string1[60] = "textftext";
-	memccpy(buffer, string1, 'f', 30);
-	printf("Result: %s\n", buffer);*/
+	printf("Пример 2\n");
+	char destination2_1[60] = "12345678";
+	char destination2_2[60] = "12345678";
+	memccpy(destination2_1, source, 'f', 30);
+	printf("Результат встроенной функции: %s\n", destination2_1);
+	my_memccpy(destination2_2, source, 'f', 30);
+	printf("Результат самописной функции: %s\n\n", destination2_2);
 
 	//3
-	char buffer[61] = "";
-	char string1[60] = "textftext";
-	my_memccpy(buffer, string1, 'f', 30);
-	printf("Result: %s\n", buffer);
-
-	//void* result;
-	//printf("Source: %s\n", string1);
-	//result = memccpy(buffer, string1, 's', 30);
-	//printf("Result: %s\n", buffer);
-
-	/*my_memccpy(buffer, string1, 'f', 60);
-	printf(buffer);*/
-
+	printf("Пример 3\n");
+	char destination3_1[60] = "12345678";
+	char destination3_2[60] = "12345678";
+	memccpy(destination3_1, source, 'q', 30);
+	printf("Результат встроенной функции: %s\n", destination3_1);
+	my_memccpy(destination3_2, source, 'q', 30);
+	printf("Результат самописной функции: %s\n\n", destination3_2);
 }
-
-
-//char* my_memccpy(char* destination, const char* source, int c, int n) {
-//	for (size_t i = 0; i < n; i++)
-//	{
-//		std::cout << "a[" << i << "]: address=" << source + i << "\tvalue=" << *(source + i) << std::endl;
-//	}
-//	return destination;
-//}
